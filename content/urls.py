2f8667c2ce597e3473a887e34fdf149ffdf37327
Emilio Ramirez <emilioramirez04@gmail.com>
2014-09-17 22:09:10 -0300

    Added the api for tastypie talk

--- content/urls.py
+++ content/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from tastypie.api import Api
+from mercadito.api import PostResource, AuthorResource
+
+v1_api = Api(api_name='v1')
+
+v1_api.register(PostResource())
+v1_api.register(AuthorResource())
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'content.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    (r'^api/', include(v1_api.urls)),
 )
---
+++ mercadito/api.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from tastypie import fields
+from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
+from mercadito.models import Post
+
+
+class AuthorResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'author'
+        excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+
+
+class PostResource(ModelResource):
+    author = fields.ForeignKey(AuthorResource, 'author')
+
+    class Meta:
+        queryset = Post.objects.all()
+        resource_name = 'post'
+        authorization = Authorization()
